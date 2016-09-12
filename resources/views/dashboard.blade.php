c143b85598408276a6fc8b2a45a7593c3b9847cc
Luke Tanprasit <tanprasitp@hotmail.co.uk>
2016-09-12 16:06:39 +0100

    Added new routes to add and remove card form customer. Customers can now update their password and account details

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -28,7 +28,7 @@
                   </div>
 
                   <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-                        <h1 class="page-header">Outstanding Orders</h1>
+                        <h1 class="page-header">Laudry</h1>
                         <div class="row">
                         </div>
                   </div>
